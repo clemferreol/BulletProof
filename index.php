34ec09f76f7f63f666c190396429ffb908d92d5a
clemferreol <cleol@9online.fr>
2017-11-29 00:29:07 +0100

    login

--- index.php
+++ index.php
@@ -6,6 +6,31 @@ require './utils/dbconnexion.php';
 ?>
 
 <form method="POST">
+<label>Pseudo: <input type="text" name="nickname_login"/></label><br/>
+<label>Mot de passe: <input type="password" name="password_login"/></label><br/>
+<input type="submit" value="Me connecter"/>
+</form>
+
+<?php
+if(isset($_POST['nickname_login']) && !empty($_POST['nickname_login']) && isset($_POST['password_login']) && !empty($_POST['password_login'])){
+    $nickname_login = htmlspecialchars($_POST['nickname_login']);
+    $password_login = htmlspecialchars($_POST['password_login']);
+    $password_hash = password_hash($password_login, PASSWORD_DEFAULT);
+
+    $q = $pdo->prepare('SELECT * FROM user WHERE nickname = :nickname');
+    $q->bindParam(':nickname', $nickname_login, PDO::PARAM_STR);
+    $q->execute();
+    $queryResults = $q->fetch(PDO::FETCH_ASSOC);
+    if(password_verify($password_login, $queryResults['password'])){
+        echo "Well done";
+    }else{
+        echo "Wrong info";
+    }
+}
+?>
+
+
+<form method="POST"
 <label>Pseudo: <input type="text" name="nickname_register"/></label><br/>
 <label>Mot de passe: <input type="password" name="password_register"/></label><br/>
 <input type="submit" value="M'inscrire"/>
@@ -22,6 +47,4 @@ if(isset($_POST['nickname_register']) && !empty($_POST['nickname_register']) && 
     $q->bindParam(':password', $password_hash, PDO::PARAM_STR);
     $register = $q->execute();
 }
-
-
 ?>
