cbab662912510e1a0a9f5e10486ff59dd86de808
clemferreol <cleol@9online.fr>
2017-12-06 23:40:39 +0100

    add correct sql

--- utils/post.sql
+++ utils/post.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost:3306
--- Généré le :  Mer 06 Décembre 2017 à 21:07
+-- Généré le :  Mer 06 Décembre 2017 à 23:35
 -- Version du serveur :  5.5.42
 -- Version de PHP :  5.6.10
 
@@ -30,3 +30,19 @@ CREATE TABLE `post` (
 --
 -- Index pour les tables exportées
 --
+
+--
+-- Index pour la table `post`
+--
+ALTER TABLE `post`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `post`
+--
+ALTER TABLE `post`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=86;
--- utils/user.sql
+++ utils/user.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost:3306
--- Généré le :  Mer 06 Décembre 2017 à 21:09
+-- Généré le :  Mer 06 Décembre 2017 à 23:39
 -- Version du serveur :  5.5.42
 -- Version de PHP :  5.6.10
 
@@ -24,8 +24,31 @@ CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `nickname` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `user`
+--
+
+INSERT INTO `user` (`id`, `nickname`, `password`) VALUES
+(19, 'NekoSam', '$2y$10$io4tr7zw2Dd8U2X14xV3CeB/KTWagP.weT3Grf5F3dYSE6O3PreUu');
 
 --
 -- Index pour les tables exportées
 --
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=20;
